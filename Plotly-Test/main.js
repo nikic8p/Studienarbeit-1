27da1ccc7eb7805775ba44cbc33d7b3d6cce16a6
nikic8p <120654238+nikic8p@users.noreply.github.com>
2024-12-12 03:55:19 +0100

    audio input und frequenzanalyse

--- Plotly-Test/main.js
+++ Plotly-Test/main.js
@@ -1,55 +1,76 @@
-import * as FFT from 'fft';
+//import * as FFT from 'fft';
 
-//const FFT = require('fft.js');
-
-const f = new FFT.FFT(256);
-
-let input = new Array(f.size);
-//const x = Math.sin()
-input.fill(0);
-
-const out = f.createComplexArray();
-
-function fillSine(arr, frequency){
-    for (let i = 0; i<arr.length; i++){
-        arr[i] = Math.cos(2 * Math.PI * frequency * i/512);
-    }
-    return arr;
-}
-let frequency = 1;
-function fourier(){
+// var constraints = { audio: true, video: false };
     
-    input = fillSine(input, frequency);
+// navigator.mediaDevices.getUserMedia(constraints).then(function (stream)
+// {
+//     var audio = document.getElementById('audioCapture');
+//     audio.srcObject = stream;
+//     // console.log(stream);
+//     audio.createAnalyser();
+
+// }).catch(function (err)
+// {
+//     console.log(err);
+// });
+
+
+// const f = new FFT.FFT(256);
+
+// let input = new Array(f.size);
+// //const x = Math.sin()
+// input.fill(0);
+
+// const out = f.createComplexArray();
+
+// function fillSine(arr, frequency){
+//     for (let i = 0; i<arr.length; i++){
+//         arr[i] = Math.cos(2 * Math.PI * frequency * i/25600);
+//     }
+//     return arr;
+// }
+// let frequency = 1;
+
+// function fourier(){
     
-    f.realTransform(out, input);
+//     input = fillSine(input, frequency);
     
-    frequency ++;
+//     f.realTransform(out, input);
+    
+//     frequency += 100;
 
-    return f.fromComplexArray(out);
-}
+//     return f.fromComplexArray(out);
+// }
 
 
-
+// Control Panel erstellen
 
 let gui = new lil.GUI();
 
+var stop = false;
+var controls = {
+    random: function(){redraw()},
+    stop: function(){stop = true;}
+}
+gui.add(controls, 'random');
+gui.add(controls, 'stop');
 
+// Auswahl des DIV Elements in der HTML file
+// hier wird mit Plotly gezeichnet
+
+var TESTER = document.getElementById('tester');
+
+// 2D Arrays für den Plot gefüllt mit 0
 
 let z_data = new Array(200).fill(0).map(()=>new Array(256).fill(0));
-//z_data = getData(z_data);
+
+// Erstellen der Plotly-js Objekte für das Zeichnen des Plots
 
 var data = [{
     z: z_data,
     type: 'surface',
     showscale: false,
     displayModeBar: false
-    // contours: {
-    // z: {
-    //     show:true,
-    //     usecolormap: true,
-    //     highlightcolor:"#42f462"
-    // }
-    // }
 }];
 
 var defaultPlotlyConfiguration = { 
@@ -72,9 +93,6 @@ var defaultPlotlyConfiguration = {
     showTips: false
 };
 
-var TESTER = document.getElementById('tester');
-
-
 var layout = {
     title: {
     text: 'Visualisierung 3D Plot mit Zufallsdaten'
@@ -82,67 +100,54 @@ var layout = {
     scene: {
         camera: {eye: {x: 1.87, y: 0.88, z: 0.64}},
         aspectmode: "manual",
-        aspectratio: {x: 1, y: 2, z: 0.2}
+        aspectratio: {x: 1, y: 2, z: 0.2},
+        xaxis: {
+            title: {
+                text: 'Frequenz',
+            }
+        },
+        yaxis: {
+            title: {
+                text: 'Zeit',
+            }
+        },
     },
     autosize: true,
     width: TESTER.clientWidth,
-    height: TESTER.clientHeight,
-    // margin: {
-    // l: 65,
-    // r: 50,
-    // b: 65,
-    // t: 50,
-    // }
+    height: TESTER.clientHeight, 
 };
+
+//Initiales Zeichnen des Plots
+
 Plotly.newPlot(TESTER, data, layout, defaultPlotlyConfiguration);
 
-var clear = false;
 
-function randomize(){
+const microphone = new Microphone()
+
+console.log(microphone);
+var samples;
+function redraw(){
+    samples = microphone.getSamples();
+
+    for (let i = 0; i<samples.length; i++){
+        samples[i] = Math.abs(samples[i] - 128);
+    }
+
+
     z_data = z_data.slice(1);
-    // z_data.push(...getData([z_data[0]]));
-    // let arr = new Array(256);
-    // for (let i = 0; i<arr.length; i++){
-    //     arr[i] = Math.sin(2 * Math.PI * frequency * i/256);
-    // }
-    // frequency += 1;
-
-    let fou = fourier();
-    console.log(fou);
-    z_data.push(...[fou]);
-    // data[0]['z'] = z_data;
-
-
-
+    z_data.push(...[samples]);
 
     var update = {
         z: [z_data]
     }
 
-    Plotly.restyle(TESTER, update, 0);
+    Plotly.update(TESTER, update, 0);
     
     setTimeout(function() {
         if (stop === true) {
             stop = false;
             return;
         }
-        randomize();
+        redraw();
       }, 20);
 }
-
-function getData(arr) {
-    
-    for (let i = 0; i< arr.length; i++){
-        for (let j = 0; j< arr[0].length; j++){
-            arr[i][j] = Math.random();
-        }
-    }
-    return arr;
-}
-var stop = false;
-var controls = {
-    random: function(){randomize()},
-    stop: function(){stop = true;}
-}
-gui.add(controls, 'random');
-gui.add(controls, 'stop');
